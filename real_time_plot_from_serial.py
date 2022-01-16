080de55b1fd7431b11e21e4c10b0d936852dc176
chibaf <chibaf@mac.com>
2022-01-16 16:35:09 +0900

    Add files via upload

--- real_time_plot_from_serial.py
+++ real_time_plot_from_serial.py
@@ -9,21 +9,23 @@ data=[data0]*10
 print(data)
 ser = serial.Serial(sys.argv[1],sys.argv[2])
 while True:
-  array=ard_serial_sub(ser)
-  if len(array)==8:
-#    data.prepend(array)
-    data.pop(-1)
-    print(len(data))
-    data.insert(0,array)
-    print(data)
-    rez = [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
-    print(rez)
-    x=range(0, 10, 1)
-    plt.clf()
-    plt.plot(x,rez[0])
-    plt.plot(x,rez[1])
-    plt.plot(x,rez[2])
-    plt.plot(x,rez[3])
-    plt.pause(0.1)
-#    exit()
+  try:
+    array=ard_serial_sub(ser)
+    if len(array)==8:
+      data.pop(-1)
+      print(len(data))
+      data.insert(0,array)
+      rez = [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
+      print(rez)
+      x=range(0, 10, 1)
+      plt.clf()
+      plt.plot(x,rez[0])
+      plt.plot(x,rez[1])
+      plt.plot(x,rez[2])
+      plt.plot(x,rez[3])
+      plt.pause(0.1)
+  except KeyboardInterrupt:
+    print(str(data))
+    print ('exiting')
+    break
 exit()
